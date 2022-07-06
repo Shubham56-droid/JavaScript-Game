830dde759a7c888a709971ac1f85bc0d117635ea
Shubham Bawankar <71594948+Shubham56-droid@users.noreply.github.com>
2022-07-06 22:49:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,105 +1,33 @@
-let playerState = 'idle';
-const dropdown = document.getElementById('animations');
-dropdown.addEventListener('change',(e)=>{
-    playerState = e.target.value;
-})
-
 const canvas = document.getElementById('canvas1');
+
 const ctx = canvas.getContext('2d');
 
-const CANVAS_WIDTH = canvas.width = 600;
-const CANVAS_HEIGHT = canvas.height = 600;
+const CANVAS_WIDTH = canvas.width = 800;
+const CANVAS_HEIGHT = canvas.height = 700;
 
+let gameSpeed = 5;
 
-const playerImage = new Image();
-playerImage.src = 'shadow_dog.png';
+const backgroundLayer1 = new Image();
+backgroundLayer1.src = 'layer-1.png';
 
-const spritWidth = 575;
-const spritHeight = 523;
+const backgroundLayer2 = new Image();
+backgroundLayer2.src = 'layer-2.png';
 
+const backgroundLayer3 = new Image();
+backgroundLayer3.src = 'layer-3.png';
 
+const backgroundLayer4 = new Image();
+backgroundLayer4.src = 'layer-4.png';
 
-let gameFrame = 0;
-const staggerFrames = 5;
-const spriteAnimation = [];
-
-const animationState = [
-    {
-        name: 'idle',
-        frames: 7,
-    },
-    {
-        name: 'jump',
-        frames: 7,
-    },
-    {
-        name: 'fall',
-        frames: 7,
-    },
-    {
-        name: 'run',
-        frames: 9,
-    },
-    {
-        name: 'dizzy',
-        frames: 11,
-    },
-    {
-        name: 'sit',
-        frames: 5,
-    },
-    {
-        name: 'roll',
-        frames: 7,
-    },
-    {
-        name: 'bite',
-        frames: 7,
-    },
-    {
-        name: 'ko',
-        frames: 12,
-    },
-    {
-        name: 'getHit',
-        frames: 4,
-    }
-
-];
-animationState.forEach((state,index)=>{
-    let frames  = {
-        loc: [],
-    }
-    for (let j = 0; j < state.frames; j++) {
-        let positionX = j * spritWidth;
-        let positionY = index * spritHeight;
-        frames.loc.push({x:positionX,y: positionY});
-    }
-    spriteAnimation[state.name] = frames;
-});
-
-console.log(spriteAnimation);
+const backgroundLayer5 = new Image();
+backgroundLayer5.src = 'layer-5.png';
 
 function animate(){
-    ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-
-    // ctx.fillRect(100,50,100,100);
-
-    // sx - sprit x axis
-    // sy - sprit y axis
-    // dx - destination x axis
-    // ctx.drawImage( image, sx, sy, sw, sh, dx, dy, dw, dh);
-
-    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimation[playerState].loc.length;
-    
-    let frameX = spritWidth * position;
-    let frameY = spriteAnimation[playerState].loc[position].y;
-
-    ctx.drawImage(playerImage,frameX ,frameY, spritWidth,spritHeight, 0, 0,spritWidth,spritHeight);
-    
-    gameFrame++;
+    ctx.drawImage(backgroundLayer1,0,0);
+    ctx.drawImage(backgroundLayer2,0,0);
+    ctx.drawImage(backgroundLayer3,0,0);
+    ctx.drawImage(backgroundLayer4,0,0);
+    ctx.drawImage(backgroundLayer5,0,0);
     requestAnimationFrame(animate);
-};
-
+}
 animate();
-
